76184c948dc2efe9a893da8606d1991edf8e4ba7
MinhVu287 <121625386+MinhVu287@users.noreply.github.com>
2024-08-22 09:26:15 +0700

    feat: create entity

--- IotBE/src/main/java/com/iot/project/controller/ActionLogController.java
+++ IotBE/src/main/java/com/iot/project/controller/ActionLogController.java
@@ -1,11 +1,17 @@
 package com.iot.project.controller;
 
+import com.iot.project.dto.response.ApiResponse;
+import com.iot.project.exception.AppException;
 import com.iot.project.service.ActionLogService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,4 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ActionLogController {
     ActionLogService actionLogService;
+
+    @GetMapping("/test")
+    public ApiResponse getting(@RequestParam String workflowName) {
+        try {
+            return ApiResponse.success(null);
+        } catch (AppException e) {
+            return ApiResponse.error(e.getErrorCode());
+        }
+    }
+
 }
--- IotBE/src/main/java/com/iot/project/entity/DataSensor.java
+++ IotBE/src/main/java/com/iot/project/entity/DataSensor.java
@@ -3,7 +3,6 @@ package com.iot.project.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.Date;
 
 @Getter
